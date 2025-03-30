202a02b14ab924ff9e44858090993aaa223a3ed7
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2025-03-30 08:21:54 +0530

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,27 +3,27 @@ import { HiDocument, HiLocationMarker, HiPhone, HiMail } from "react-icons/hi";
 
 const Footer = () => {
     return (
-        <footer className="w-full  text-white/80 py-8 px-4 md:px-8 lg:px-16">
+        <footer className="flex justify-center items-center w-full text-slate-200 h-[300px] py-8 px-4 md:px-8 lg:px-16">
             <div className="max-w-6xl mx-auto">
-                <div className="flex flex-col items-center gap-6 text-sm">
+                <div className="relative z-20 flex flex-col items-center gap-6 text-xl font-extralight">
                     {/* Contact Information */}
                     <div className="flex flex-col md:flex-row items-center gap-4 md:gap-8">
                         {/* Address */}
                         <div className="flex items-center gap-2">
-                            <HiLocationMarker className="w-4 h-4" />
-                            <span>Ichapur, Kolkata - 743144, West Bengal</span>
+                            <HiLocationMarker className="w-10 h-10   " />
+                            <span className={'bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text  text-transparent'}>Ichapur, Kolkata - 743144, West Bengal</span>
                         </div>
 
                         {/* Phone */}
                         <div className="flex items-center gap-2">
-                            <HiPhone className="w-4 h-4" />
-                            <span>9874353532</span>
+                            <HiPhone className="w-10 h-10" />
+                            <span className="bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text  text-transparent" >9874353532</span>
                         </div>
 
                         {/* Email */}
                         <div className="flex items-center gap-2">
-                            <HiMail className="w-4 h-4" />
-                            <span>Chakrobortypritam12@gmail.com</span>
+                            <HiMail className="w-10 h-10" />
+                            <span className="bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text  text-transparent">Chakrobortypritam12@gmail.com</span>
                         </div>
                     </div>
 
